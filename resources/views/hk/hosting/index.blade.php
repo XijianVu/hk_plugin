773e3b97a235fb0a5cbf2256126054b4ca751cb4
Thu Vu <thuvm25@gmail.com>
2024-11-05 15:01:30 +0700

    fix position text

--- resources/views/hk/hosting/index.blade.php
+++ resources/views/hk/hosting/index.blade.php
@@ -38,7 +38,7 @@
             <div class="card shadow-sm h-100">
                 <div class="card-body">
                     <h3 class="card-title">Wordpress VIP</h3>
-                    <p class="card-text text-center">Quản trị toàn diện</p>
+                    <p class="card-text text-center text-nowrap">Quản trị toàn diện</p>
                     <h6 class="text-muted">Chỉ từ</h6>
                     <h3 class="text-primary font-weight-bold">381.000₫</h3>
                     <p class="text-muted text-center">/tháng</p>
@@ -53,7 +53,7 @@
             <div class="card shadow-sm h-100">
                 <div class="card-body">
                     <h3 class="card-title">SEO</h3>
-                    <p class="card-text text-center">Tốt cho SEO</p>
+                    <p class="card-text text-center text-nowrap">Tốt cho SEO</p>
                     <h6 class="text-muted">Chỉ từ</h6>
                     <h3 class="text-primary font-weight-bold">142.000₫</h3>
                     <p class="text-muted text-center">/tháng</p>
@@ -68,7 +68,7 @@
             <div class="card shadow-sm h-100">
                 <div class="card-body">
                     <h3 class="card-title">Nodejs</h3>
-                    <p class="card-text text-center">Truy cập web dễ dàng</p>
+                    <p class="card-text text-center text-nowrap">Truy cập web dễ dàng</p>
                     <h6 class="text-muted">Chỉ từ</h6>
                     <h3 class="text-primary font-weight-bold">74.000₫</h3>
                     <p class="text-muted text-center">/tháng</p>
@@ -83,7 +83,7 @@
             <div class="card shadow-sm h-100">
                 <div class="card-body">
                     <h3 class="card-title">Python</h3>
-                    <p class="card-text">Nền tảng lập trình ứng dụng</p>
+                    <p class="card-text text-center text-nowrap">Nền tảng lập trình ứng dụng</p>
                     <h6 class="text-muted text-center">Chỉ từ</h6>
                     <h3 class="text-primary font-weight-bold">122.000₫</h3>
                     <p class="text-muted text-center">/tháng</p>
