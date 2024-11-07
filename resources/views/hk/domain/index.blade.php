74907fcf598b0bf22d50e556cf7e42f89f54a33a
Phan Van Hoang Anh <pvhoanganh240@gmail.com>
2024-11-08 03:19:18 +0700

    feat: update modal UI

--- resources/views/hk/domain/index.blade.php
+++ resources/views/hk/domain/index.blade.php
@@ -518,33 +518,43 @@
                                                             <li class="nav-item"> <a data-toggle="pill" href="#paypal" class="nav-link "> <i class="fab fa-paypal mr-2"></i> Paypal </a> </li>
                                                             <li class="nav-item"> <a data-toggle="pill" href="#net-banking" class="nav-link "> <i class="fas fa-mobile-alt mr-2"></i> Net Banking </a> </li>
                                                         </ul>
-                                                    </div> <!-- End -->
-                                                    <!-- Credit card form content -->
+                                                    </div>
+
                                                     <div class="tab-content">
-                                                        <!-- credit card info-->
                                                         <div id="credit-card" class="tab-pane fade show active pt-3">
                                                             <form role="form" onsubmit="event.preventDefault()">
-                                                                <div class="form-group"> <label for="username">
-                                                                        <h6>Card Owner</h6>
-                                                                    </label> <input type="text" name="username" placeholder="Card Owner Name" required class="form-control "> </div>
-                                                                <div class="form-group"> <label for="cardNumber">
-                                                                        <h6>Card number</h6>
+                                                                <div class="form-group"> 
+                                                                    <label class="ms-0" for="username">
+                                                                        <h5 class="text-bold">Chủ Thẻ</h5>
+                                                                    </label> 
+                                                                    <input type="text" name="username" placeholder="Tên chủ thẻ" required class="form-control "> 
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label class="ms-0" for="cardNumber">
+                                                                        <h5 class="text-bold">Số Thẻ</h5>
                                                                     </label>
-                                                                    <div class="input-group"> <input type="text" name="cardNumber" placeholder="Valid card number" class="form-control " required>
-                                                                        <div class="input-group-append"> <span class="input-group-text text-muted"> <i class="fab fa-cc-visa mx-1"></i> <i class="fab fa-cc-mastercard mx-1"></i> <i class="fab fa-cc-amex mx-1"></i> </span> </div>
+                                                                    <div class="input-group"> 
+                                                                        <input type="text" name="cardNumber" placeholder="Số thẻ hợp lệ" class="form-control " required>
+                                                                        <div class="input-group-append">
+                                                                            <span class="input-group-text text-muted">
+                                                                                <i class="fab fa-cc-visa mx-1"></i>
+                                                                                <i class="fab fa-cc-mastercard mx-1"></i>
+                                                                                <i class="fab fa-cc-amex mx-1"></i>
+                                                                            </span>
+                                                                        </div>
                                                                     </div>
                                                                 </div>
                                                                 <div class="row">
                                                                     <div class="col-sm-8">
                                                                         <div class="form-group"> <label><span class="hidden-xs">
-                                                                                    <h6>Expiration Date</h6>
+                                                                                    <h5 class="text-bold">Expiration Date</h5>
                                                                                 </span></label>
                                                                             <div class="input-group"> <input type="number" placeholder="MM" name="" class="form-control" required> <input type="number" placeholder="YY" name="" class="form-control" required> </div>
                                                                         </div>
                                                                     </div>
                                                                     <div class="col-sm-4">
                                                                         <div class="form-group mb-4"> <label data-toggle="tooltip" title="Three digit CV code on the back of your card">
-                                                                                <h6>CVV <i class="fa fa-question-circle d-inline"></i></h6>
+                                                                                <h5 class="text-bold">CVV <i class="fa fa-question-circle d-inline"></i></h5>
                                                                             </label> <input type="text" required class="form-control"> </div>
                                                                     </div>
                                                                 </div>
